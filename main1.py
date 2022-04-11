f9db1e7225a8637afb94472670fc6da334fe2eaa
sagar barde <bardesagar@gmail.com>
2022-04-11 21:32:09 +0530

    first flask file

---
+++ main1.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__) #constructor of the flask
+
+@app.route('/')
+def base_route():
+    return "Welcome to Praxis"
+
+
+if __name__ == "__main__":
+    app.run()
